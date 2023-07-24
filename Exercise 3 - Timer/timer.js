fcb3bc079403d694708ce548246a08ea30f57801
AristideI <a.isingizwe@alustudent.com>
2023-07-24 11:45:38 +0200

    Latest Update

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,49 @@
-console.log("Exercise 3 - Timer");
+let display = document.querySelector(".display")
+let minus = document.querySelector(".minus")
+let plus = document.querySelector(".plus")
+let reset = document.querySelector(".reset")
+let start = document.querySelector(".start")
+let stop = document.querySelector(".stop")
+
+let displayNum = 89
+display.textContent = displayNum
+
+plus.addEventListener("click", function(){
+    displayNum++
+    display.textContent = displayNum
+})
+minus.addEventListener("click", function(){
+    displayNum--
+    display.textContent = displayNum
+})
+
+start.addEventListener("click", function(){
+    start.classList.toggle("hidden")
+    stop.classList.toggle("hidden")
+    counting()
+    
+    
+})
+
+let counting = setInterval(function () {
+    if (displayNum === 0) {
+        stop()
+    }
+    else {
+        displayNum--
+        display.textContent = displayNum
+    }
+    counter.textContent = countValue
+
+
+}, 1000)
+
+stop.addEventListener("click", function(){
+     start.classList.toggle("hidden")
+    stop.classList.toggle("hidden")
+
+})
+
+function stop() {
+    clearInterval(counting)
+}
